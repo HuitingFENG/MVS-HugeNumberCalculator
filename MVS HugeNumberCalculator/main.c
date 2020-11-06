cae5e968a06cc53c5c968269e46903c027359b35
yasmrn <73698442+yasmrn@users.noreply.github.com>
2020-11-06 19:45:06 +0100

    Update main.c

--- MVS HugeNumberCalculator/main.c
+++ MVS HugeNumberCalculator/main.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "IHMHugeNumber/ScanHugeNumber.h"
-#include "IHMHugeNumber/ShowHugeNumber.h"
+#include "UseHugeNumber/Operator/HugeFloatOperator.h"
+#include "UseHugeNumber/Operator/HugeIntOperator.h"
+#include "UseHugeNumber/Operator/HugeUnsignedIntOperator.h"
 
 int main (void) {
-    return EXIT_SUCCESS;
+HugeInt* hugeNumber = createHugeIntFromString
+("123456789012345678901234567890"); printHugeInt (hugeNumber); hugeNumber = createHugeIntFromString
+("0000000123456789012345678901234567890"); printHugeInt (hugeNumber);
 }
